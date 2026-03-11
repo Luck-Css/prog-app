d9ba77d283df58afbbf0b8cca29c7c46e1c45646
Luck-Css <lucascharff@proton.me>
2026-03-11 19:30:44 -0300

    Rename some folders

--- 01-Components/src/components/Exer1.jsx
+++ 01-Components/src/components/Exer1.jsx
@@ -26,7 +26,7 @@ function Exer1() {
         <div className='grid place-items-center h-100'>
             <button on onClick={calcTabuada} class="group relative h-12 overflow-hidden overflow-x-hidden rounded-md bg-neutral-950 px-8 py-2 text-neutral-50"><span class="relative z-10">Calcular Tabuada</span><span class="absolute inset-0 overflow-hidden rounded-md"><span class="absolute left-0 aspect-square w-full origin-center -translate-x-full rounded-full bg-emerald-600 transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150"></span></span></button>
             {tabuada.map((item, index) => (
-                <li key={index}>{item}</li>
+                <p key={index}>{item}</p>
             ))}
         </div>
     </div>
